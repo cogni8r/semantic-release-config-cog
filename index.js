781046616aa435f7c20871731a8f288a620d7948
cogni8r <57598620+cogni8r@users.noreply.github.com>
2021-09-10 12:08:02 +0500

    feat: add `semantic-release` config

---
+++ index.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+const path = require('path');
+
+/**
+ *
+ * @param isMonorepository {boolean}
+ */
+function getConfig(isMonorepository) {
+    const npmPlugins = isMonorepository
+        ? getNpmPlugins()
+        : ["@semantic-release/npm"];
+
+    return {
+        branches: [
+            "main"
+        ],
+        plugins: [
+            "@semantic-release/commit-analyzer",
+            "@semantic-release/release-notes-generator",
+            [
+                "@semantic-release/changelog",
+                {
+                    "changelogTitle": "# Changelog",
+                    "changelogFile": "CHANGELOG.md"
+                }
+            ],
+            [
+                "@semantic-release/git",
+                {
+                    "assets": ["CHANGELOG.md"]
+                }
+            ],
+            ["@semantic-release/exec", {
+                "prepareCmd": "npx replace '0.0.0-development' '${nextRelease.version}' ./packages -r --include=\"package.json\""
+            }],
+            ...npmPlugins,
+            "@semantic-release/github"
+        ]
+    };
+}
+
+function getNpmPlugins() {
+    const plugins = [["@semantic-release/exec", {
+        "prepareCmd": "npx replace '0.0.0-development' '${nextRelease.version}' ./packages -r --include=\"package.json\""
+    }]];
+
+    const packages = getPackages();
+
+    for (const packageName of packages) {
+        plugins.push(["@semantic-release/npm", {
+            "pkgRoot": `packages/${packageName}`
+        }]);
+    }
+
+    return plugins;
+}
+
+/**
+ *
+ * @returns {string[]}
+ */
+function getPackages() {
+    const cwd = process.cwd();
+
+    const packagesFolder = path.join(cwd, 'packages');
+
+    const directoryItems = fs.readdirSync(packagesFolder, {withFileTypes: true});
+
+    return getFoldersOnly(directoryItems);
+}
+
+/**
+ * @param folderContent {Dirent[]}
+ * @returns {string[]}
+ */
+function getFoldersOnly(folderContent) {
+    const result = [];
+
+    for (const item of folderContent) {
+        if (item.isDirectory()) {
+            result.push(item.name);
+        }
+    }
+
+    return result;
+}
+
+module.exports = getConfig;
+
+console.log(getConfig(false));
